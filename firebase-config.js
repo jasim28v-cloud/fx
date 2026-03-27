6d4dabc0d4ad09f0aad63ce2e0e241edeaafb2f5
jasim28v-cloud <jasim28v@gmail.com>
2026-03-28 00:03:04 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 import { getDatabase, ref, set, get, push, onValue, update, remove, query, orderByChild, equalTo } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-database.js";
-import { getFirestore, doc, setDoc, getDoc, collection, addDoc, query as fsQuery, where, getDocs, updateDoc, deleteDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDoc, collection, addDoc, query as fsQuery, where, getDocs, updateDoc, deleteDoc, onSnapshot, orderBy as fsOrderBy } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 
-// Firebase Configuration
+// Firebase Config
 const firebaseConfig = {
     apiKey: "AIzaSyAQEHv1K69ZtA48l1TpqUfAIJlmM20gZyA",
     authDomain: "tlgr-1436a.firebaseapp.com",
@@ -15,26 +15,25 @@ const firebaseConfig = {
     measurementId: "G-K4W3BBTV0G"
 };
 
-// Initialize Firebase
+// Cloudinary Config
+const CLOUDINARY_CONFIG = {
+    cloudName: "dnillsbmi",
+    uploadPreset: "ekxzvogb"
+};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const firestore = getFirestore(app);
 
-// Admin credentials
 const ADMIN_EMAIL = "jasim28v@gmail.com";
 const ADMIN_CODE = "vv2314vv";
 
 export { 
-    auth, 
-    database, 
-    firestore,
+    auth, database, firestore,
     ref, set, get, push, onValue, update, remove, query, orderByChild, equalTo,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged,
-    doc, setDoc, getDoc, collection, addDoc, fsQuery, where, getDocs, updateDoc, deleteDoc, onSnapshot,
-    ADMIN_EMAIL,
-    ADMIN_CODE
+    createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile,
+    doc, setDoc, getDoc, collection, addDoc, fsQuery, where, getDocs, updateDoc, deleteDoc, onSnapshot, fsOrderBy,
+    ADMIN_EMAIL, ADMIN_CODE,
+    CLOUDINARY_CONFIG
 };
